ab88ff4cbc2818f98c0c2d1e6d349158ccc23ae1
Marina Bashkirova <marina.bashkirova.2398@gmail.com>
2019-02-17 18:21:57 +0300

    init ommit

---
+++ lesson2/table.py
@@ -0,0 +1,15 @@
+# table.py
+value = int(input('Введите атомный номер элемента '))
+if value == 3:
+    print('Это Литий Li')
+elif value == 25:
+    print('Это Марганец Mn')
+elif value == 80:
+    print('Это Ртуть Hg')
+elif value == 17:
+    print('Это Хлор Cl')
+else:
+    print('Данный элемент неизвестен!')
+
+        
+    
